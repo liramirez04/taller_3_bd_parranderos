73c41a3999e0eb36613d878bad25a33529bf907b
Lina Isabella Ramírez Mora <157720470+liramirez04@users.noreply.github.com>
2026-05-11 10:47:50 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pymongo import MongoClient
+from datetime import datetime
+import os
+import uvicorn
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+#os.environ para despliegue. Descomente cuando ya probó todo local.
+#client = MongoClient(os.environ["MONGO_URI"])
+# TODO: conectarse al cluster Admonsis  
+client = MongoClient("mongodb://ISIS2304F24202610:PugPXO9A7ywk@157.253.236.88:8087")
+
+#client = MongoClient("")
+# TODO: conectarse a la base de datos Admonsis  
+# db = client["ISIS*******"]
+db = client["ISIS2304F24202610"]
+
+
+@app.get("/")
+def inicio():
+    return {"estado": "API funcionando correctamente"}
+
+@app.get('/bares/{bar_id}/comentarios')
+def get_comentarios(bar_id: int):
+    comentarios = db["comentarios"].find_one({"bar_id":bar_id},{"_id":0}) 
+    return comentarios or {}
+
+@app.post('/bares/{bar_id}/comentarios')
+def post_comentario(bar_id: int, datos: dict):
+    datos['bar_id'] = bar_id
+    datos['fecha']  = datetime.now().isoformat()
+    db["comentarios"].insert_one(datos)
+    return {'mensaje': 'Comentario guardado'}
+
+# TODO: implementar GET /bares/{bar_id}/eventos
+# Debe retornar todos los eventos del bar desde la colección 'eventos'
+@app.get('/bares/{bar_id]/eventos}')
+def get_eventos(bar_id: int):
+    eventos = db["eventos"].find({"bar_id": bar_id}, {"_id": 0}) 
+    return eventos or {}
+
+# TODO: implementar POST /bares/{bar_id}/eventos  
+# Debe insertar el evento en la colección 'eventos'
+# Recuerde agregar bar_id y fecha_creacion al documento antes de insertar
+@app.post('/bares/{bar_id}/eventos')
+def post_evento(bar_id: int, datos: dict):
+    datos['bar_id'] = bar_id
+    datos['fecha_creacion']  = datetime.now().isoformat()
+    db["eventos"].insert_one(datos)
+    return {'mensaje': 'Evento guardado'}
