031dd8e01d360596eb49bbad5e6a92f1e2e8efac
Lina Isabella Ramírez Mora <157720470+liramirez04@users.noreply.github.com>
2026-05-14 12:22:04 -0500

    Ajustes retorno

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def inicio():
 @app.get('/bares/{bar_id}/comentarios')
 def get_comentarios(bar_id: int):
     comentarios = list(db["comentarios"].find({"bar_id":bar_id},{"_id":0}))
-    return comentarios or {}
+    return comentarios or []
 
 @app.post('/bares/{bar_id}/comentarios')
 def post_comentario(bar_id: int, datos: dict):
@@ -46,7 +46,7 @@ def post_comentario(bar_id: int, datos: dict):
 @app.get('/bares/{bar_id}/eventos}')
 def get_eventos(bar_id: int):
     eventos = list(db["eventos"].find({"bar_id": bar_id}, {"_id": 0}))
-    return eventos or {}
+    return eventos or []
 
 # TODO: implementar POST /bares/{bar_id}/eventos  
 # Debe insertar el evento en la colección 'eventos'
